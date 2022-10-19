ed610808d9c61e1bc59e967443c7672929939557
calluume <cdevans98@gmail.com>
2022-10-19 16:25:10 +0100

    Added banner options and improved logos
    - Added new banner options
    - Added logo to the nav bar
    - Updated low quality charity logo images and added transparent background
    - Improved scrolling logo wheel

--- js/js.js
+++ js/js.js
@@ -1,35 +1,5 @@
 noTrustees = 4
-logos = [["childrenyoungpeople", 5],
-         ["education", 11],
-         ["health", 11],
-         ["socialequity", 10],
-         ["overseas", 8]]
-
-function addScrollingImages(noImages) {
-    images = []
-    while (images.length < noImages && images.length < 45) {
-        folder = Math.floor(Math.random() * 5)
-        image = Math.floor(Math.random() * logos[folder][1])
-        imageURL = `${logos[folder][0]}/logo${image}.png`
-        if (images.indexOf(imageURL) < 0) {
-            images.push(imageURL)
-            document.getElementById('scroll-inner-div').innerHTML += `<img class="scroll-img" src="images/logo_wheel/${imageURL}">`
-        }
-    }
-    document.getElementById('scroll-inner-div').style.animation = `img-scroll ${noImages*3}s linear infinite`
-}
-
-function addCharityImages() {
-    for (let i = 0; i < logos.length; i++) {
-        folder = logos[i][0]
-        if (document.getElementById(folder+'-charities')) {
-            for (let j = 0; j < logos[i][1]; j++) {
-                imageURL = `${folder}/logo${j}.png`
-                document.getElementById(folder+'-charities').innerHTML += `<img src="images/logo_wheel/${imageURL}">`
-            }
-        }
-    }
-}
+scrollDuration = 3
 
 function showTrusteeInfo(trusteeID) {
     cardOpen = false;
@@ -69,8 +39,18 @@ window.onload = function() {
             $('#nav-div').removeClass('show');
         }
     }
-    if (document.getElementById('scroll-inner-div')) {
-        addScrollingImages(20)
+    
+    scrollingDivs = document.getElementsByClassName('scroll-inner-div')
+    for (let i = 0; i < scrollingDivs.length; i++) {
+        images = scrollingDivs[i].children
+        if (images.length >= 4) {
+            for (let j = 0; j < 4; j++) {
+                clone = images[j].cloneNode(true)
+                if (j >= 2) { clone.classList.add('hide-scroll') }
+                scrollingDivs[i].appendChild(clone)
+            }
+            scrollingDivs[i].style.animation = `img-scroll ${images.length * scrollDuration}s linear -${images.length * scrollDuration}s infinite`
+        }
     }
-    addCharityImages()
+    
 }
